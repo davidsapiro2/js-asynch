1f6ee17614ed8b3c8c2d22b206cf7165bb42f564
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-03-18 16:27:17 -0700

    did number 2

--- js-asynchronous.js
+++ js-asynchronous.js
@@ -2,6 +2,10 @@
 
 const NUMBERS_API_BASE_URL = "http://numbersapi.com";
 
+/** makes a request to the Numbers API
+ * to get trivia about your favorite number.
+ */
+
 async function showNumberTrivia(number) {
   const resp = await fetch(`http://numbersapi.com/${number}`, {
     headers: {"Content-Type": "application/json"}
@@ -12,3 +16,23 @@ async function showNumberTrivia(number) {
 }
 
 showNumberTrivia(5);
+
+
+/** requests trivia about four different numbers,
+ * log the piece of trivia for the winning number
+ * as soon as one request returns
+*/
+
+async function showNumberRace(num1, num2, num3, num4) {
+  const firstNum = fetch(`http://numbersapi.com/${num1}?json`);
+  const secondNum = fetch(`http://numbersapi.com/${num2}?json`);
+  const thirdNum = fetch(`http://numbersapi.com/${num3}?json`);
+  const fourthNum = fetch(`http://numbersapi.com/${num4}?json`);
+
+  const answerPromise = await Promise.race([firstNum, secondNum, thirdNum, fourthNum])
+  const data = await answerPromise.json()
+
+  console.log("Number fact: ", data.text);
+}
+
+showNumberRace(1124, 43, 266, 123);
